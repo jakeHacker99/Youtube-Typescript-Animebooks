41e9e0aaede846be7334fe1c83e092af58e48bb1
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-24 22:12:08 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+function App() {
+  return <AppContainer className="App">Anime Books </AppContainer>;
+}
+
+export default App;
